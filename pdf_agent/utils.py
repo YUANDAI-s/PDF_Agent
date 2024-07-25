142a22bc7eb181270dc0f1c35df6e835db3114b4
YUANDAI-s <149780263+YUANDAI-s@users.noreply.github.com>
2024-07-25 17:42:17 +0800

    Update utils.py

--- pdf_agent/utils.py
+++ pdf_agent/utils.py
@@ -7,7 +7,7 @@ from langchain.chains import ConversationalRetrievalChain
 import streamlit as st
 
 def qa_agent(openai_api_key, memory, upload_file, question):
-    model = ChatOpenAI(model = "gpt-3.5-turbo",openai_api_key = "sk-28YdMVkIZR7s8w5892BbD37f87904bBa95D8F568F61038Ff",openai_api_base = "https://api.aigc369.com/v1")
+    model = ChatOpenAI(model = "gpt-3.5-turbo",openai_api_key = openai_api_key,openai_api_base = "https://api.aigc369.com/v1")
     file_content = upload_file.read()
     temp_file_path = "temp.pdf"
     with open(temp_file_path,"wb") as temp_file:

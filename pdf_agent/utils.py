792ce3b4219da289a90e1470d584fd95c2e8f097
YUANDAI-s <149780263+YUANDAI-s@users.noreply.github.com>
2024-07-25 17:33:44 +0800

    Update utils.py

--- pdf_agent/utils.py
+++ pdf_agent/utils.py
@@ -5,36 +5,28 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
 import streamlit as st
-import time  # 导入 time 模块
 
 def qa_agent(openai_api_key, memory, upload_file, question):
-    model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key, openai_api_base="https://api.aigc369.com/v1")
+    model = ChatOpenAI(model = "gpt-3.5-turbo",openai_api_key = openai_api_key,openai_api_base = "https://api.aigc369.com/v1")
     file_content = upload_file.read()
     temp_file_path = "temp.pdf"
-    with open(temp_file_path, "wb") as temp_file:
+    with open(temp_file_path,"wb") as temp_file:
         temp_file.write(file_content)
-    
     loader = PyPDFLoader(temp_file_path)
     docs = loader.load()
-    
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,  # 减小块大小
-        chunk_overlap=50,
-        separators=["\n", "。", "！", "？", "，", "、", ""]
+        chunk_size=2000,
+        chunk_overlap=100,
+        separators=["\n","。","！","？","，","、",""]
     )
     texts = text_splitter.split_documents(docs)
-    
-    embeddings_model = OpenAIEmbeddings(openai_api_key=openai_api_key, openai_api_base="https://api.aigc369.com/v1")
-    
-    # 直接使用 from_documents 方法创建FAISS索引
-    db = FAISS.from_documents(texts, embeddings_model)
-    
+    embeddings_model = OpenAIEmbeddings(openai_api_key = openai_api_key,openai_api_base = "https://api.aigc369.com/v1")
+    db = FAISS.from_documents(texts,embeddings_model)
     retriever = db.as_retriever()
-    
-    qa = ConversationalRetrievalChain.from_llm(
+    qa =ConversationalRetrievalChain.from_llm(
         llm=model,
-        retriever=retriever,
-        memory=memory
+        retriever =retriever,
+        memory = memory
     )
-    response = qa.invoke({"chat_history": memory, "question": question})
+    response = qa.invoke({"chat_history":memory,"question":question})
     return response

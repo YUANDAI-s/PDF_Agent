9a5461791090ee5ee077294ee2d4b4e116921536
YUANDAI-s <149780263+YUANDAI-s@users.noreply.github.com>
2024-10-27 19:01:37 +0800

    Add files via upload

--- pdf_agent/utils.py
+++ pdf_agent/utils.py
@@ -4,12 +4,10 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
-import streamlit as st
+
 
 def qa_agent(openai_api_key, memory, upload_file, question):
-    model = ChatOpenAI(model = "gpt-3.5-turbo",
-                       openai_api_key = openai_api_key,
-                       openai_api_base = "https://api.aigc369.com/v1")
+    model = ChatOpenAI(model = "gpt-3.5-turbo",openai_api_key = openai_api_key,openai_api_base = "https://api.chatanywhere.tech/v1")
     file_content = upload_file.read()
     temp_file_path = "temp.pdf"
     with open(temp_file_path,"wb") as temp_file:
@@ -17,12 +15,12 @@ def qa_agent(openai_api_key, memory, upload_file, question):
     loader = PyPDFLoader(temp_file_path)
     docs = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=2000,
-        chunk_overlap=100,
+        chunk_size=1000,
+        chunk_overlap=50,
         separators=["\n","。","！","？","，","、",""]
     )
     texts = text_splitter.split_documents(docs)
-    embeddings_model = OpenAIEmbeddings(openai_api_key = openai_api_key,openai_api_base = "https://api.aigc369.com/v1")
+    embeddings_model = OpenAIEmbeddings(openai_api_key = openai_api_key,openai_api_base = "https://api.chatanywhere.tech/v1")
     db = FAISS.from_documents(texts,embeddings_model)
     retriever = db.as_retriever()
     qa =ConversationalRetrievalChain.from_llm(
@@ -32,3 +30,13 @@ def qa_agent(openai_api_key, memory, upload_file, question):
     )
     response = qa.invoke({"chat_history":memory,"question":question})
     return response
+
+
+
+
+
+
+
+
+
+
